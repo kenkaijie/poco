16d3c96874bf16d4c70d2801d090e478154a68ae
Kenneth Ng <ngkaijie@hotmail.com>
2025-04-21 21:15:19 +1000

    feat: add basic integration level tests

---
+++ samples/multiple-coroutines/sample_multiple_coroutines.c
@@ -0,0 +1,52 @@
+/*!
+ * @file
+ * @brief In this example, 1024 coroutines are created, each one
+ *        will produce 2 numbers, then terminate.
+ * 
+ * Due to the order defined within the basic scheduler, the output will just be the
+ * numbers 0-2047.   
+ */
+#include <poco/coro.h>
+#include <poco/scheduler/basic_scheduler.h>
+#include <stdio.h>
+
+#define CORO_COUNT (1024) // number of coroutines to spawn
+#define STACK_SIZE (1024)
+
+// As we are creating a large number of homogenous coroutines, we just do it as an array.
+
+static coro_t coroutines[CORO_COUNT];
+static uint32_t coroutine_stacks[CORO_COUNT][STACK_SIZE] __attribute__((__aligned__(8)));
+
+// We are using the same entry for all coroutines.
+void producer_task(coro_t *coro, void* context)
+{
+    intptr_t starting_number = (intptr_t)context;
+    printf("%d\n", starting_number);
+    coro_yield(coro);
+    printf("%d\n", starting_number + CORO_COUNT);
+    coro_yield(coro);
+    return;
+}
+
+int main() {
+
+    coro_t* tasks[CORO_COUNT] = {0};
+
+    for (size_t i=0; i < CORO_COUNT; ++i)
+    {
+        tasks[i] = coro_create_static(&coroutines[i], producer_task, (void *)(uintptr_t)i, &(coroutine_stacks[i][0]), sizeof(coroutine_stacks[i]));
+    }
+
+    basic_scheduler_t * scheduler = basic_scheduler_create(tasks, CORO_COUNT);
+
+    if (scheduler == NULL)
+    {
+        printf("Failed to create scheduler\n");
+        return -1;
+    }
+
+    basic_scheduler_run(scheduler);
+
+    return 0;
+}
---
+++ tests/capture_stdout.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# This script is used to capture the output of a command and save it to a file.
+# Used to allow ctest to check the output.
+
+# Check if the correct number of arguments is provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <command> <output_file>"
+    exit 1
+fi
+# Assign arguments to variables
+command=$1
+output_file=$2
+# Execute the command and capture the output
+
+$command 1> $output_file 2> /dev/null
