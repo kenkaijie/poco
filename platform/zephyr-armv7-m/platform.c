83df751f9f2ed86e36940c34d2224787301c8262
Kenneth Ng <ngkaijie@hotmail.com>
2025-08-07 21:24:53 +1000

    style: run format

--- platform/zephyr-armv7-m/platform.c
+++ platform/zephyr-armv7-m/platform.c
@@ -6,12 +6,12 @@
 #include <stdarg.h>
 #include <stddef.h>
 
-// We modify the context to think it originates from the function provided. That way the next
-// time we resume this context, it will magically appear inside.
+// We modify the context to think it originates from the function provided. That way the
+// next time we resume this context, it will magically appear inside.
 void platform_make_context(platform_context_t *ucp, void (*func)(void *, void *),
                            void *context1, void *context2) {
     // Pre-populate first 4 with arguments used within the entry point (+ some others).
-	// This is okay as we know the first 2 will be the correct values.
+    // This is okay as we know the first 2 will be the correct values.
 
     // Deviating from standard as I couldn't get va_args to work.
     ucp->uc_mcontext.r0 = (uintptr_t)context1;
