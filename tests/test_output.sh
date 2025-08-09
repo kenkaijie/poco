34133034780fc399078d0a04960c5abcebaee32b
Kenneth Ng <ngkaijie@hotmail.com>
2025-08-10 00:38:32 +1000

    feat: simplify testing

--- tests/test_output.sh
+++ tests/test_output.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
-# This script is used to capture the output of a command and save it to a file.
-# Used to allow ctest to check the output.
+# This script is used to capture the output of a command and save it to a file and to
+# compare that file with an expected output file.
 #
 # SPDX-FileCopyrightText: Copyright contributors to the poco project.
 # SPDX-License-Identifier: MIT
 
 # Check if the correct number of arguments is provided
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <command> <output_file>"
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <command> <output_file> <expected_output>"
     exit 1
 fi
 # Assign arguments to variables
 command=$1
 output_file=$2
+expected_output=$3
 # Execute the command and capture the output
 
 $command 1> $output_file 2> /dev/null
 
-cat $output_file
+diff $output_file $expected_output
